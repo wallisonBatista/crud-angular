b2eab894c470e33ff183f88d9ced9f406c446ec2
wallisonBatista <wallisongbatista@gmail.com>
2022-12-25 18:56:41 -0300

    criado nova coluna com os botões e ícones para ações de adicionar, editar e excluir curso

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
@@ -15,12 +16,14 @@ import { CoursesService } from '../services/courses.service';
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category']
+  displayedColumns = ['name', 'category', 'actions']
   // displayedColumns = ['_id', 'name', 'category']
 
   constructor(
     public coursesService: CoursesService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.courses$ = this.coursesService.list()
       .pipe(
@@ -37,8 +40,12 @@ export class CoursesComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
 
+  ngOnInit(): void { }
+
+  onAdd() {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
+
 
 }
