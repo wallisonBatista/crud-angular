02a05f5afe5a42aaeaba83edc193da12e932b853
wallisonBatista <wallisongbatista@gmail.com>
2023-01-08 16:54:46 -0300

    criando curso e exibindo, criando lógica do location para voltar a página ao cancelar ou ao salvar o novo curso

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,7 +16,8 @@ export class CourseFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar) {
+    private snackBar: MatSnackBar,
+    private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -25,10 +27,19 @@ export class CourseFormComponent implements OnInit {
   ngOnInit(): void { }
 
   onSubmit() {
-    this.service.save(this.form.value).subscribe(result => console.log(result), error => this.onError());
+    this.service.save(this.form.value)
+      .subscribe(result => this.onSucess(), error => this.onError());
   }
 
-  onCancel() { }
+  onCancel() {
+    this.location.back();
+  }
+
+  private onSucess() {
+    this.snackBar.open('Curso salvo com sucesso', '', { duration: 5000 });
+    this.onCancel();
+  }
+
   private onError() {
     this.snackBar.open('Erro ao salvar o curso.', '', { duration: 5000 });
   }
